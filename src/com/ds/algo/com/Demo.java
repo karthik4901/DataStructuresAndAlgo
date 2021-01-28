9ecce54e8380b614c90e853586b868c6fdb5db0c
karth <karth@LAPTOP-3Q1JCPDS.home>
2021-01-28 11:06:27 -0600

    Producer-Consumer Thread Java

--- src/com/ds/algo/com/Demo.java
+++ src/com/ds/algo/com/Demo.java
@@ -3,8 +3,38 @@ package com.ds.algo.com;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		final ProducerConsumer pc = new ProducerConsumer();
+		
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				
+				try {
+					pc.produce();
+				}catch(InterruptedException e) {
+					
+					e.printStackTrace();
+					
+				}
+				
+			}
+		});
 
-	}
+	
+	
+	Thread t2 = new Thread(new Runnable() {
+		
+		@Override
+		public void run() {
+			
+			try {
+				pc.consume();
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	});
 
+ }
 }
